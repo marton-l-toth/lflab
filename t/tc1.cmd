85fd16bcba9ad3e18df781aa09b3e597df14100c
Marton L. Toth <m.l.toth@gmail.com>
2016-03-10 17:46:38 +0100

    test case list

---
+++ t/tc1.cmd
@@ -0,0 +1,15 @@
+# simple test aid script, use with -iord 1
+N.!e$ce2
+W.!e.box.fe.s0+ch
+W.e2.box.fe.s0+ch
+W.e2.box.fe.s0-c
+W.e2.box.trk.trk-in-trk
+X.e2.box.trk.trk-in-trk$p1
+W.t1.emptytrk
+W.!e.1
+W.e2.3
+W.e2.3.3
+X.!e.3.3$PT
+X.!e.3.3$PF
+cW
+W.t1.k.g
