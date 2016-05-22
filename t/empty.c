cf32fdb5888f8b0ca20c7dd08ceb9b71e7cf2761
Marton L. Toth <m.l.toth@gmail.com>
2016-05-22 17:06:55 +0200

    dev.env.: added empty C src file to repo

---
+++ t/empty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+#define FP0(S) 		fprintf(stderr, S "\n");
+#define FP1(S,X) 	fprintf(stderr, S "\n", (X));
+#define FP2(S,X,Y) 	fprintf(stderr, S "\n", (X),(Y));
+#define FP3(S,X,Y,Z) 	fprintf(stderr, S "\n", (X),(Y),(Z));
+#define FP4(S,X,Y,Z,W) 	fprintf(stderr, S "\n", (X),(Y),(Z),(W));
+
+int main(int ac, char** av) {
+	FP2("hello %s %d", *av, ac);
+	return 0;
+}
